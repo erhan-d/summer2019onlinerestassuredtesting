82309636e2137f6ce53b9625c50b6d5e73be4fe4
erhan-d <dolapcierhan@hotmail.com>
2020-01-23 11:59:45 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -0,0 +1,21 @@
+package com.automation.tests.day2;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSTests {
+
+    private String baseURI="http://ec2-54-205-50-162.compute-1.amazonaws.com:1000/ords/hr";
+
+    @Test
+    public void test1(){
+
+        Response response=given().
+                get( baseURI+"/employees");
+        System.out.println(response.getBody().asString());
+        assertEquals(200,response.getStatusCode());
+        System.out.println(response.prettyPrint());
+    }
+}
