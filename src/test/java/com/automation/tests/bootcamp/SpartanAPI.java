3657252368426ce04c48045aa7268c8bac5ce55a
erhan-d <dolapcierhan@hotmail.com>
2020-02-28 19:57:14 -0500

    after stash commit

--- src/test/java/com/automation/tests/bootcamp/SpartanAPI.java
+++ src/test/java/com/automation/tests/bootcamp/SpartanAPI.java
@@ -4,11 +4,18 @@ public class SpartanAPI {
   
    public static void main(String[] args) {
       System.out.println("Holy");
+
       System.out.println("HOly Stash");
      
+
+
+       System.out.println("Moly");
+
         
     }
   
+
+
 
 
 }
